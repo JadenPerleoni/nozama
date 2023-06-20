44bb3043c1594a4451c462f1040d7e5ee5e3c2cb
JadenPerleoni <jadenperleoni@gmail.com>
2023-06-20 08:57:16 -0700

    added password hashing, soon implementing jwt

--- server/models/userInfo.js
+++ server/models/userInfo.js
@@ -4,17 +4,18 @@ const userSchema = mongoose.Schema({
   email: {
     type: String,
     required: true,
+    unique: true,
   },
   username: {
     type: String,
     required: true,
+    unique: true,
   },
   password: {
     type: String,
     required: true,
   },
 });
- const UserInfo = mongoose.model("UserInfo", userSchema);
+const UserInfo = mongoose.model("UserInfo", userSchema);
 
- export default UserInfo;
-
+export default UserInfo;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,32 @@
 import express from "express";
-import { register } from "../controllers/index.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import UserInfo from "../models/userInfo.js";
 
 const router = express.Router();
 
-//localhost:4000/
 
-router.post("/register", register);
+// Registration route
+router.post("/register", async (req, res) => {
+  try {
+    const { email, username, password } = req.body;
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create the new user
+    const user = new UserInfo({
+      email,
+      username,
+      password: hashedPassword,
+    });
+
+    // Save user to db
+    await user.save();
+    res.status(201).json({ message: "User successfully created" });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to register user" });
+  }
+});
 
 export default router;
