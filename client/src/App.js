fcaf2562020823d7e90db03eb2d9bde4c39267ed
JadenPerleoni <jadenperleoni@gmail.com>
2023-07-07 10:47:14 -0700

    routing nonsense

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Register from "./components/Register/Register";
 import Login from "./components/Login/Login";
@@ -8,7 +8,26 @@ import Cart from "./components/Cart/Cart";
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <div>
+      <nav>
+        <div className="nav-links">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/products">Market</Link>
+          </li>
+          <li>
+            <Link to="/cart">Cart</Link>
+          </li>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </div>
+      </nav>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/register" element={<Register></Register>}></Route>
@@ -16,7 +35,7 @@ function App() {
         <Route path="/products" element={<Products></Products>}></Route>
         <Route path="/cart" element={<Cart></Cart>}></Route>
       </Routes>
-    </BrowserRouter>
+    </div>
   );
 }
 
